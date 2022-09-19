4bafb1045a33a4eca7eb11f5e2cfd9f854e9373b
nullyng <nullyng@gmail.com>
2022-09-19 10:18:24 +0900

    S07P22D110-111 [feat] : 데이터 입력 drawer 생성
    - MUI drawer를 이용해 데이터 입력 틀 생성
    
    - 참조 url : https://mui.com/material-ui/react-drawer/01~

--- src/components/Main/Input/Input.js
+++ src/components/Main/Input/Input.js
@@ -1,3 +1,36 @@
-function Input() { }
+import { Drawer, IconButton } from "@mui/material";
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+import { useState } from "react";
+
+import "styles/Main/Input.scss";
+
+function Input() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setIsDrawerOpen(true);
+  }
+
+  const handleDrawerClose = () => {
+    setIsDrawerOpen(false);
+  }
+
+  return (
+    <div className="left-drawer">
+      <IconButton className="left-drawer__button" onClick={handleDrawerOpen}>
+        <KeyboardDoubleArrowRightIcon />
+      </IconButton>
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={isDrawerOpen}>
+        <IconButton onClick={handleDrawerClose}>
+          <KeyboardDoubleArrowLeftIcon />
+        </IconButton>
+      </Drawer>
+    </div>
+  )
+}
 
 export default Input;
