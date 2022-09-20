a68ecf274d92d2a8b9db419f0a081d8a3be1049d
신예인 <syi0511@naver.com>
2022-09-20 10:17:24 +0900

    [merge] : back-develop <- S07P22D110-39/feature/animalhospital/get

---
+++ back/demo/src/main/java/com/salmanhaljido/demo/animalhospital/service/AnimalHospitalService.java
@@ -0,0 +1,8 @@
+package com.salmanhaljido.demo.animalhospital.service;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public interface AnimalHospitalService {
+    void getAnimalHospitalData() throws IOException;
+}
