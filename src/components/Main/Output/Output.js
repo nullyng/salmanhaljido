40b334d2af3e18304fb24057058df54152c8d453
박진경 <wls0828rud@naver.com>
2022-09-20 16:51:37 +0900

    S07P22D110-117 [feat] : 추천 지역 chip 코드 추가
    - 출력 데이터의 추천 지역 부분 chip 코드 추가

--- src/components/Main/Output/Output.js
+++ src/components/Main/Output/Output.js
@@ -1,41 +1,59 @@
 import { Drawer, IconButton } from "@mui/material";
-import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
-import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 import { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import "styles/Main/Output.scss";
+import RcmdList from "./Rcmd/RcmdList";
 
 function Output() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const handleDrawerOpen = () => {
     setIsDrawerOpen(true);
-  }
+  };
 
   const handleDrawerClose = () => {
     setIsDrawerOpen(false);
-  }
+  };
+
+  // 스타일
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#e94560",
+      },
+      secondary: {
+        main: "#0F3460",
+      },
+    },
+  });
 
   return (
-    <div className="right-drawer">
-      <div className="right-drawer__button">
-        <IconButton onClick={handleDrawerOpen}>
-          <KeyboardDoubleArrowLeftIcon />
-        </IconButton>
-      </div>
-      <Drawer
-        className="right-drawer__inner"
-        variant="persistent"
-        anchor="right"
-        open={isDrawerOpen}>
-        <div className="right-drawer__inner__button" >
-          <IconButton onClick={handleDrawerClose}>
-            <KeyboardDoubleArrowRightIcon />
+    <ThemeProvider theme={theme}>
+      <div className="right-drawer">
+        <div className="right-drawer__button">
+          <IconButton onClick={handleDrawerOpen}>
+            <KeyboardDoubleArrowLeftIcon />
           </IconButton>
         </div>
-      </Drawer>
-    </div>
-  )
+        <Drawer
+          className="right-drawer__inner"
+          variant="persistent"
+          anchor="right"
+          open={isDrawerOpen}
+        >
+          <div className="right-drawer__inner__button">
+            <IconButton onClick={handleDrawerClose}>
+              <KeyboardDoubleArrowRightIcon />
+            </IconButton>
+          </div>
+          <RcmdList />
+        </Drawer>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default Output;
