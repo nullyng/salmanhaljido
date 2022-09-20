bfbbc68ea3309004971b83fa8dff4b13c1b996fd
이재영 <wodud5381@naver.com>
2022-09-20 13:48:56 +0900

    S07P22D110-96/feature/landing/detail 사용자는 서비스 상세 정보를 확인할 수 있다.

--- src/components/Landing/Detail/Service.js
+++ src/components/Landing/Detail/Service.js
@@ -1,3 +1,16 @@
-function Service() { }
+import "styles/Landing/Landing.scss";
+import ServiceBox from "./ServiceBox";
+
+function Service() {
+    return(
+        <div className="service__introduce">
+            <div className="service__introduce--textbox">
+                <div className="service__bigfont">살만할지도</div><div className="service__font">는</div>
+            </div>
+            <div className="service__font">이런 서비스를 지원하고 있어요.</div>
+            <ServiceBox/>
+        </div>
+    )
+}
 
 export default Service;
