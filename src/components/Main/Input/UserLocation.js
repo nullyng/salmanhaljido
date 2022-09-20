818db09b742ae931c0be431d2a20323671b97c72
nullyng <nullyng@gmail.com>
2022-09-20 13:44:38 +0900

    S07P22D110-113 [feat] : 법정동 코드 조회 API 요청 코드 수정
    - Select에서 선택한 값을 토대로 구/군 리스트를 요청
    - 구/군 리스트 출력 시 제일 첫번째 인덱스 값은 출력하지 않도록 함 (시/도 그자체이므로)
    - 구/군 리스트 출력 시 받아온 구/군을 ' '로 분할하여 뒤쪽의 구/군 이름만 사용 (ex. 서울특별시 종로구에서 종로구만 사용)

--- src/components/Main/Input/UserLocation.js
+++ src/components/Main/Input/UserLocation.js
@@ -1,4 +1,10 @@
-import { FormControl, InputLabel, MenuItem, Select, ThemeProvider } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  ThemeProvider,
+} from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -7,27 +13,34 @@ import theme from "components/common/theme.js";
 
 function UserLocation() {
   const [sidoList, setSidoList] = useState([]);
-  const [sido, setSido] = useState(0);
+  const [sido, setSido] = useState("");
   const [gugunList, setGugunList] = useState([]);
-  const [gugun, setGugun] = useState(0);
+  const [gugun, setGugun] = useState("");
 
   useEffect(() => {
     // 초기 한 번만 시/도 목록을 받아온다.
-    axios.get('https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=*00000000')
+    axios
+      .get(
+        "https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=*00000000"
+      )
       .then((res) => {
-        setSidoList(res.data.regcodes);
-      })
-  }, [])
+        setSidoList(() => res.data.regcodes);
+      });
+  }, []);
 
   const handleSidoChange = (event) => {
     // 시/도가 바뀔 때 해당하는 시/도의 구/군 목록을 받아온다.
     setSido(event.target.value);
 
-    const sidoCode = sido.slice(0, 2) + "**000000";
-    axios.get(`https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=${sidoCode}`)
+    // 시/도 코드 2글자 + **000000
+    const sidoCode = event.target.value.substring(0, 2) + "**000000";
+    axios
+      .get(
+        `https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=${sidoCode}`
+      )
       .then((res) => {
         setGugunList(res.data.regcodes);
-      })
+      });
   };
 
   const handleGugunChange = (event) => {
@@ -46,11 +59,7 @@ function UserLocation() {
         <ThemeProvider theme={theme}>
           <FormControl className="user-location__content__sido" fullWidth>
             <InputLabel>시/도</InputLabel>
-            <Select
-              value={sido}
-              label="시/도"
-              onChange={handleSidoChange}
-            >
+            <Select value={sido} label="시/도" onChange={handleSidoChange}>
               {sidoList.map((item, index) => {
                 return (
                   <MenuItem key={index} value={item.code}>
@@ -62,15 +71,16 @@ function UserLocation() {
           </FormControl>
           <FormControl className="user-location__content__gugun" fullWidth>
             <InputLabel>구/군</InputLabel>
-            <Select
-              value={gugun}
-              label="구/군"
-              onChange={handleGugunChange}
-            >
+            <Select value={gugun} label="구/군" onChange={handleGugunChange}>
               {gugunList.map((item, index) => {
+                // 0번째 인덱스는 항상 시/도 그 자체이므로 건너뛴다.
+                if (index == 0) return;
+                // 받아온 구/군 이름을 ' '로 분할하여 뒤쪽의 구/군만 사용한다.
+                // 받아온 구/군 이름 예시 : 서울특별시 종로구
+                const arr = item.name.split(" ", 2);
                 return (
                   <MenuItem key={index} value={item.code}>
-                    {item.name}
+                    {arr[1]}
                   </MenuItem>
                 );
               })}
@@ -79,7 +89,7 @@ function UserLocation() {
         </ThemeProvider>
       </div>
     </div>
-  )
+  );
 }
 
 export default UserLocation;
