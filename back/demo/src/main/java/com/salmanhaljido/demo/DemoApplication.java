159774f7c5f5e6cbc773040ba34bf8db3eaff6f6
Sangmeeeee <goodman1223@naver.com>
2022-09-21 20:12:27 +0900

    [fix] : CrimeServiceImpl 삭제, 의존성 제거

--- back/demo/src/main/java/com/salmanhaljido/demo/DemoApplication.java
+++ back/demo/src/main/java/com/salmanhaljido/demo/DemoApplication.java
@@ -1,27 +1,13 @@
 package com.salmanhaljido.demo;
 
-import com.salmanhaljido.demo.domain.caraccident.service.CarAccidentServiceImpl;
-import com.salmanhaljido.demo.domain.crime.service.CrimeServiceImpl;
-import com.salmanhaljido.demo.domain.drugstore.service.DrugStoreServiceImpl;
-import com.salmanhaljido.demo.domain.hospital.service.HospitalServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DemoApplication.class, args);
-		try{
-			CrimeServiceImpl s = new CrimeServiceImpl();
-			s.getCrime();
-		}catch(Exception e ){
-			e.printStackTrace();
-		}
-
 	}
 
 }
