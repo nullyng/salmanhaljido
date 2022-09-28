b0bd1fa0f179baa009e13ea537ad538a564a45cf
이재영 <wodud5381@naver.com>
2022-09-28 09:27:29 +0900

    S07P22D110-107/feature/main/category 사용자는 카테고리를 선택할 수 있다.

--- src/components/Main/Input/CategoryChip.js
+++ src/components/Main/Input/CategoryChip.js
@@ -1,14 +1,19 @@
+import { useDispatch } from "react-redux";
+import { delCategory } from "modules/CategorySet"
+
+import Avatar from '@mui/material/Avatar';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 
-function CategoryChip() { 
+function CategoryChip({CategoryName, CategoryLevel}) { 
+  const dispatch = useDispatch();
   const handleDelete = () => {
-    console.info('You clicked the delete icon.');
+    dispatch(delCategory(CategoryName))
   };
 
   return (
     <Stack direction="row" spacing={1}>
-      <Chip label="임시 칩" variant="outlined" onDelete={handleDelete} />
+      <Chip avatar={<Avatar>{CategoryLevel}</Avatar>} label={CategoryName} variant="outlined" onDelete={handleDelete} />
     </Stack>
   );
 }
