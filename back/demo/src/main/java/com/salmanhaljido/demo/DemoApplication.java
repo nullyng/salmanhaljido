6c5638ca3bc4318cb6cadc447d9727a2e0f70f78
이기종 <rl00822@naver.com>
2022-09-30 01:26:14 +0900

    [fix] : docker-compose 파일 수정

--- back/demo/src/main/java/com/salmanhaljido/demo/DemoApplication.java
+++ back/demo/src/main/java/com/salmanhaljido/demo/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.salmanhaljido.demo;
 
-import com.salmanhaljido.demo.domain.news.service.NewServiceImpl;
-import com.salmanhaljido.demo.domain.news.service.NewsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
