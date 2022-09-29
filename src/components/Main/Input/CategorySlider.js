ea05d56e6ab4768ff6bab6b83f2155abec706e96
nullyng <nullyng@gmail.com>
2022-09-29 21:48:53 +0900

    [merge] : front-develop ← S07P22D110-107/feature/main/category

--- src/components/Main/Input/CategorySlider.js
+++ src/components/Main/Input/CategorySlider.js
@@ -1,38 +1,33 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addCategory, delCategory } from "modules/CategorySet"
-import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
+import { useDispatch, useSelector } from "react-redux";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
 
-const valueList = [
-  '선택안함',
-  '하',
-  '중',
-  '상'
-]
+import { addCategory, delCategory } from "modules/category";
+
+const valueList = ["선택안함", "하", "중", "상"];
 
 const label = {
-  1:'하',
-  2:'중',
-  3:'상'
-}
+  1: "하",
+  2: "중",
+  3: "상",
+};
 
-function CategorySlider({CategoryName}) {
-
-  const [ value , setValue ] = useState (0) ;    
+function CategorySlider({ CategoryName }) {
+  const [value, setValue] = useState(0);
+  const myCategoryList = useSelector((state) => state.category.myCategoryList);
   const dispatch = useDispatch();
-  
+
   // value 변화가 일어나면
-  const changeValue = ( event , newvalue ) => {  
-      console.log(newvalue)
-      // 변한 값으로 재설정   
-      setValue ( newvalue ) ;
-      // 변한값이 상, 중, 하(3, 2, 1)이면 리덕스 이용해서 칩 추가
-      if (newvalue !== 0) {
-        dispatch(addCategory(CategoryName, label[newvalue]));
+  const changeValue = (event, newValue) => {
+    // 변한 값으로 재설정
+    setValue(newValue);
+    // 변한값이 상, 중, 하(3, 2, 1)이면 리덕스 이용해서 칩 추가
+    if (newValue !== 0) {
+      dispatch(addCategory(CategoryName, newValue));
       // 0 이면 삭제
-      } else {
-        dispatch(delCategory(CategoryName))
+    } else {
+      dispatch(delCategory(CategoryName));
     }
   };
 
@@ -42,8 +37,8 @@ function CategorySlider({CategoryName}) {
         defaultValue={0}
         valueLabelFormat={(label) => valueList[label]}
         step={1}
-        valueLabelDisplay="auto" 
-        value={value} 
+        valueLabelDisplay="auto"
+        value={myCategoryList[CategoryName] || 0}
         onChange={changeValue}
         marks
         min={0}
