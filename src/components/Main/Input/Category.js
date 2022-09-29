dadebecc1e5511c01a8438a1fb236a1923be5b3f
nullyng <nullyng@gmail.com>
2022-09-29 19:03:21 +0900

    [merge] : S07P22D110-107/feature/main/category ← front-develop
    Squashed commit of the following:
    
    commit e2357334539f61ca56a6477fb254086845ff9db3
    Merge: 0d4b1ae a421c92
    Author: 김주영 <wn00315@naver.com>
    Date:   Thu Sep 29 18:38:14 2022 +0900
    
        [merge] : front-develop ← S07P22D110-158/feature/main/user-input
    
        Merge branch 'S07P22D110-158/feature/main/user-input' into 'front-develop'
    
        S07P22D110-158/feature/main/user-input 사용자는 사용자 데이터를 입력할 수 있다.
    
        See merge request s07-bigdata-dist-sub2/S07P22D110!77
    
    commit a421c929ba02041ee032309ac98d606f5830cc3a
    Author: 김주영 <wn00315@naver.com>
    Date:   Thu Sep 29 18:38:14 2022 +0900
    
        S07P22D110-158/feature/main/user-input 사용자는 사용자 데이터를 입력할 수 있다.
    
    commit 0d4b1ae2a0b97e8dbe8291d435046e04592117f6
    Merge: 20364ad 02e5611
    Author: 김주영 <wn00315@naver.com>
    Date:   Thu Sep 29 17:21:14 2022 +0900
    
        [merge] : front-develop ← S07P22D110-114/feature/main/output
    
        Merge branch 'S07P22D110-114/feature/main/output' into 'front-develop'
    
        S07P22D110-114/feature/main/output 사용자는 출력 받은 데이터를 볼 수 있다.
    
        See merge request s07-bigdata-dist-sub2/S07P22D110!76
    
    commit 02e5611a4a9bb79aba9015a00bd904de47e3c3be
    Author: 김주영 <wn00315@naver.com>
    Date:   Thu Sep 29 17:21:14 2022 +0900
    
        S07P22D110-114/feature/main/output 사용자는 출력 받은 데이터를 볼 수 있다.
    
    commit 20364add1eb6ba497da69f94e36ecc03015eee34
    Merge: a3f8fca 2f60cb6
    Author: 김주영 <wn00315@naver.com>
    Date:   Wed Sep 28 23:56:31 2022 +0900
    
        [merge] : front-develop ← S07P22D110-107/feature/main/category
    
        Merge branch 'S07P22D110-107/feature/main/category' into 'front-develop'
    
        S07P22D110-107/feature/main/category 사용자는 카테고리를 선택할 수 있다.
    
        See merge request s07-bigdata-dist-sub2/S07P22D110!72
    
    commit 2f60cb6d72350491a5e681defa2d533b5578c2be
    Author: 이재영 <wodud5381@naver.com>
    Date:   Wed Sep 28 23:56:31 2022 +0900
    
        S07P22D110-107/feature/main/category 사용자는 카테고리를 선택할 수 있다.

--- src/components/Main/Input/Category.js
+++ src/components/Main/Input/Category.js
@@ -1,120 +1,124 @@
 import CustomTooltip from "components/common/CustomTooltip";
 
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import CategoryPanel from "./CategoryPanel";
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 1 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 1 }}>{children}</Box>}
+    </div>
+  );
+}
+
 TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
 };
 
 function a11yProps(index) {
-    return {
+  return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-    };
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
 }
 
 const CategoryList = {
-    "교통" : ["대중교통 이용률", "대중교통 정류장", "전기차 충전소", "공용, 민영주차장"],
-    "재난" : ["민방위 대피소"],
-    "안전" : ["교통사고", "화재", "범죄", "어린이 보호구역", "여성안심"],
-    "의료" : ["의료기관", "약국"],
-    "반려동물" : ["동물병원", "동물미용"],
-    "교육" : ["유치원", "초, 중, 고등학교", "학원 및 교습소"],
-    "문화" : ["미술관, 박물관", "영화관", "도서관", "공연장", "스포츠시설"],
-    "생활" : ["음식점", "공원", "마트, 시장", "장애인 편의시설"],
-  };
-    
+  교통: [
+    "대중교통 이용률",
+    "대중교통 정류장",
+    "전기차 충전소",
+    "공용, 민영주차장",
+  ],
+  재난: ["민방위 대피소"],
+  안전: ["교통사고", "화재", "범죄", "어린이 보호구역", "여성안심"],
+  의료: ["의료기관", "약국"],
+  반려동물: ["동물병원", "동물미용"],
+  교육: ["유치원", "초, 중, 고등학교", "학원 및 교습소"],
+  문화: ["미술관, 박물관", "영화관", "도서관", "공연장", "스포츠시설"],
+  생활: ["음식점", "공원", "마트, 시장", "장애인 편의시설"],
+};
+
 function Category() {
-    const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-        };
-    const handleChange2 = (event, newValue) => {
-        setValue(newValue + 4);
-        };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const handleChange2 = (event, newValue) => {
+    setValue(newValue + 4);
+  };
 
-    const tooltipMessage =
+  const tooltipMessage =
     "8개의 카테고리에서 필요한 세부카테고리를 골라보세요. 중요도는 상, 중, 하로 나뉘어져 있고 필요없을 시 선택안함 또는 내 카테고리에서 X를 클릭하면 삭제할 수 있어요.";
-      
-    return(
-        <div className="category">
-            <div className="category__title">
-                <h2 className="category__title__text">카테고리 설정</h2>
-                <div className="user-location__title__tooltip">
-                    <CustomTooltip content={tooltipMessage} />
-                </div>
-            </div>
-            <Box sx={{ width: '100%' }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }} className="categorybox">
-                <Tabs value={value} onChange={handleChange}>
-                    <Tab label="교통" {...a11yProps(0)}/>
-                    <Tab label="재난" {...a11yProps(1)}/>
-                    <Tab label="안전" {...a11yProps(2)}/>
-                    <Tab label="의료" {...a11yProps(3)}/>
-                </Tabs>
-                <Tabs value={value - 4} onChange={handleChange2}>
-                    <Tab label="반려동물" {...a11yProps(4)}/>
-                    <Tab label="문화" {...a11yProps(5)}/>
-                    <Tab label="교육" {...a11yProps(6)}/>
-                    <Tab label="생활" {...a11yProps(7)}/>
-                </Tabs>
-            </Box>
-            <TabPanel value={value} index={0}>
-                <CategoryPanel arr={CategoryList.교통}/>
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                <CategoryPanel arr={CategoryList.재난}/>
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                <CategoryPanel arr={CategoryList.안전}/>
-            </TabPanel>
-            <TabPanel value={value} index={3}>
-                <CategoryPanel arr={CategoryList.의료}/>
-            </TabPanel>
-            <TabPanel value={value} index={4}>
-                <CategoryPanel arr={CategoryList.반려동물}/>
-            </TabPanel>
-            <TabPanel value={value} index={5}>
-                <CategoryPanel arr={CategoryList.교육}/>
-            </TabPanel>
-            <TabPanel value={value} index={6}>
-                <CategoryPanel arr={CategoryList.문화}/>
-            </TabPanel>
-            <TabPanel value={value} index={7}>
-                <CategoryPanel arr={CategoryList.생활}/>
-            </TabPanel>
-            </Box>
+
+  return (
+    <div className="category">
+      <div className="category__title">
+        <h2 className="category__title__text">카테고리 설정</h2>
+        <div className="user-location__title__tooltip">
+          <CustomTooltip content={tooltipMessage} />
         </div>
-    )
- }
+      </div>
+      <Box sx={{ width: "100%" }}>
+        <Box
+          sx={{ borderBottom: 1, borderColor: "divider" }}
+          className="categorybox"
+        >
+          <Tabs value={value} onChange={handleChange}>
+            <Tab label="교통" {...a11yProps(0)} />
+            <Tab label="재난" {...a11yProps(1)} />
+            <Tab label="안전" {...a11yProps(2)} />
+            <Tab label="의료" {...a11yProps(3)} />
+          </Tabs>
+          <Tabs value={value - 4} onChange={handleChange2}>
+            <Tab label="반려동물" {...a11yProps(4)} />
+            <Tab label="문화" {...a11yProps(5)} />
+            <Tab label="교육" {...a11yProps(6)} />
+            <Tab label="생활" {...a11yProps(7)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <CategoryPanel arr={CategoryList.교통} />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <CategoryPanel arr={CategoryList.재난} />
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <CategoryPanel arr={CategoryList.안전} />
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <CategoryPanel arr={CategoryList.의료} />
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          <CategoryPanel arr={CategoryList.반려동물} />
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          <CategoryPanel arr={CategoryList.교육} />
+        </TabPanel>
+        <TabPanel value={value} index={6}>
+          <CategoryPanel arr={CategoryList.문화} />
+        </TabPanel>
+        <TabPanel value={value} index={7}>
+          <CategoryPanel arr={CategoryList.생활} />
+        </TabPanel>
+      </Box>
+    </div>
+  );
+}
 
 export default Category;
--- src/components/Main/Output/Rcmd/RcmdList.js
+++ src/components/Main/Output/Rcmd/RcmdList.js
@@ -1,10 +1,41 @@
+import { useEffect } from "react";
+
 import CustomTooltip from "components/common/CustomTooltip";
 import RcmdChip from "components/Main/Output/Rcmd/RcmdChip.js";
 import "styles/Main/Output.scss";
 
-function RcmdList() {
+function RcmdList({
+  rcmdData,
+  currRegion,
+  onSetCurrRegion,
+  onSetStatistics,
+  onSetRealEstate,
+}) {
   const tooltipMessage =
     "추천 지역 검색을 통해 추천 지역을 확인할 수 있어요.\n추천 지역은 순위가 높은 순으로 표시됩니다.\n해당 지역의 자세한 정보를 보고 싶다면, 원하는 지역을 클릭해 보세요!";
+
+  useEffect(() => {
+    let el;
+    let elClassName;
+
+    if (JSON.stringify(currRegion) === "{}") return;
+
+    el = document.getElementsByClassName(`${currRegion.code}`)[0];
+    elClassName = el.className;
+    el.className += " selected";
+
+    return () => {
+      el.className = elClassName;
+    };
+  }, [currRegion]);
+
+  const handleClick = (data) => {
+    // 현재 선택한 지역, 해당 지역의 통계 및 부동산 정보 저장
+    onSetCurrRegion(data);
+    onSetStatistics(data.stat);
+    onSetRealEstate(data.re);
+  };
+
   return (
     <div className="region">
       <div className="region__title">
@@ -14,22 +45,30 @@ function RcmdList() {
         </div>
       </div>
       <div className="region__cntr">
-        {/* 아마 내가 설정한 지역의 추천 */}
         <div>
-          <RcmdChip />
-          <RcmdChip />
-          <RcmdChip />
-          <RcmdChip />
-          <RcmdChip />
+          <h3>선택한 지역 내</h3>
         </div>
-        {/* 설정 지역 이외 추천 지역 */}
         <div>
-          <RcmdChip />
-          <RcmdChip />
-          <RcmdChip />
-          <RcmdChip />
-          <RcmdChip />
+          <h3>선택한 지역 외</h3>
         </div>
+        {rcmdData.length === 0 ? (
+          <p className="region__cntr--no-data">추천 지역을 검색해주세요.</p>
+        ) : (
+          <div className="region__cntr--data">
+            {rcmdData.map((data, index) => {
+              return (
+                <RcmdChip
+                  key={index}
+                  index={index}
+                  name={data.name}
+                  code={data.code}
+                  score={data.score}
+                  handleClick={() => handleClick(data)}
+                />
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
