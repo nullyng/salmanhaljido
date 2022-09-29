6af5c4342289b19ab2f245bdc0ada56396ee9580
nullyng <nullyng@gmail.com>
2022-09-29 16:40:47 +0900

    S07P22D110-113 [feat] : 입력 관련 redux 코드 추가 및 시군구 드롭다운 삭제
    - 지역, 사용자 정보, 카테고리, 가격에 대한 정보를 저장하는 redux 코드 추가
    - 지역 설정 컴포넌트에서 시군구 드롭다운 삭제

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import Input from "components/Main/Input/Input";
 import CustomMap from "components/Main/Map/CustomMap";
 import Output from "components/Main/Output/Output";
+import { setCategories, setPrice, setRegion, setUserInfo } from "modules/input";
 import { setCurrMap, setMapData, setMarkers } from "modules/map";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -9,10 +10,17 @@ function MainPage() {
   const mapData = useSelector((state) => state.map.mapData);
   const markers = useSelector((state) => state.map.markers);
 
+  const region = useSelector((state) => state.input.region);
+
   const dispatch = useDispatch();
   const onSetCurrMap = (currMap) => dispatch(setCurrMap(currMap));
   const onSetMapData = (mapData) => dispatch(setMapData(mapData));
   const onSetMarkers = (markers) => dispatch(setMarkers(markers));
+
+  const onSetRegion = (region) => dispatch(setRegion(region));
+  const onSetUserInfo = (userInfo) => dispatch(setUserInfo(userInfo));
+  const onSetCategories = (categories) => dispatch(setCategories(categories));
+  const onSetPrice = (price) => dispatch(setPrice(price));
 
   return (
     <div>
@@ -20,7 +28,12 @@ function MainPage() {
         currMap={currMap}
         onSetMapData={onSetMapData}
         markers={markers}
+        region={region}
         onSetMarkers={onSetMarkers}
+        onSetRegion={onSetRegion}
+        onSetUserInfo={onSetUserInfo}
+        onSetCategories={onSetCategories}
+        onSetPrice={onSetPrice}
       />
       <Output />
       <CustomMap
