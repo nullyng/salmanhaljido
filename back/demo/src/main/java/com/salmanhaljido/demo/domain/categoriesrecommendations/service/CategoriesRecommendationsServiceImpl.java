626872e62643d5b1f932a1463277dba90422744a
이기종 <rl00822@naver.com>
2022-09-30 12:13:10 +0900

    [fix] : NumberFormatException 오류 수정

--- back/demo/src/main/java/com/salmanhaljido/demo/domain/categoriesrecommendations/service/CategoriesRecommendationsServiceImpl.java
+++ back/demo/src/main/java/com/salmanhaljido/demo/domain/categoriesrecommendations/service/CategoriesRecommendationsServiceImpl.java
@@ -86,7 +86,7 @@ public class CategoriesRecommendationsServiceImpl implements CategoriesRecommend
             File writeFile = new File(dataPath + "categories.json");
             FileOutputStream fileOutputStream = new FileOutputStream(writeFile);
             JSONObject valueJSON = new JSONObject();
-            valueJSON.put("mainCategory", mainCategory);
+            valueJSON.put("mainCategory", mainCategory+"");
             valueJSON.put("academy", 0);
             valueJSON.put("animalHospital", 0);
             valueJSON.put("animalBeauty", 0);
@@ -160,7 +160,7 @@ public class CategoriesRecommendationsServiceImpl implements CategoriesRecommend
 
                 List<String> categoryList = new ArrayList<>();
                 for (Row row : ds.select("mainCategory").collectAsList()) {
-                    categoryList.add(row.get(0).toString());
+                    categoryList.add(row.toString().substring(1, 5));
                 }
                 List<String> ratingList = new ArrayList<>();
                 for (Row row : ds.select("rating").collectAsList()) {
@@ -169,6 +169,7 @@ public class CategoriesRecommendationsServiceImpl implements CategoriesRecommend
                 for (String col : ds.columns()) {
                     if (col.equals("_id")) continue;
                     else if (col.equals("mainCategory")) continue;
+                    else if (col.equals("rating")) continue;
 
                     FileOutputStream ratingFileOutputStream = new FileOutputStream(ratingFile);
                     int cnt=0;
@@ -179,10 +180,11 @@ public class CategoriesRecommendationsServiceImpl implements CategoriesRecommend
 
                         if (!categoryList.get(i).equals(mainCategory)) continue;
                         cnt++;
-                        if (s.equals("0")) zeroCnt++;
-                        if (rowList.get(i).get(0).toString().equals("0")) continue;
-
-                        String rowString = categoryList.get(i) + " " + rowList.get(i).get(0).toString() + " " + ratingList.get(i);
+                        if (s.equals("0")) {
+                            zeroCnt++;
+                        }
+                        double d = Math.round(Double.parseDouble(ratingList.get(i)));
+                        String rowString = categoryList.get(i) + " " + (int)Math.round(Double.parseDouble(s)) + " " + (int)d;
                         ratingFileOutputStream.write(rowString.getBytes(StandardCharsets.UTF_8));
                         ratingFileOutputStream.write("\r\n".getBytes(StandardCharsets.UTF_8));
                     }
@@ -193,14 +195,18 @@ public class CategoriesRecommendationsServiceImpl implements CategoriesRecommend
                     JavaRDD<String[]> rawRatings = rawData.map(str -> str.split(" "));
                     JavaRDD<Rating> ratings = rawRatings.map(arr -> new Rating(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]), Double.parseDouble(arr[2])));
                     ratings.cache();
-                    MatrixFactorizationModel model = ALS.train(JavaRDD.toRDD(ratings), 3, 1, 0.01);
+                    MatrixFactorizationModel model = ALS.train(JavaRDD.toRDD(ratings), 1, 1, 0.01);
                     int K = 3;
                     Rating[] topKRecs = model.recommendProducts(Integer.parseInt(mainCategory), K);
                     String s = "";
                     int num = topKRecs[0].product();
-                    if (num == 1) s = "low";
+                    if(topKRecs[0].rating() == topKRecs[1].rating()){
+                        num = Math.max(topKRecs[0].product(), topKRecs[1].product());
+                    }
+                    if( num==0) continue;
+                    else if (num == 1) s = "low";
                     else if (num == 2) s = "middle";
-                    else s = "high";
+                    else if(num==3)s = "high";
                     returnRatingList.add(CategoriesDto.of(col, s));
                 }
 
@@ -235,7 +241,7 @@ public class CategoriesRecommendationsServiceImpl implements CategoriesRecommend
 
                 List<String> categoryList = new ArrayList<>();
                 for (Row row : ds.select("mainCategory").collectAsList()) {
-                    categoryList.add(row.get(0).toString());
+                    categoryList.add(row.toString().substring(1, 5));
                 }
                 List<String> ratingList = new ArrayList<>();
                 for (Row row : ds.select("rating").collectAsList()) {
@@ -245,6 +251,7 @@ public class CategoriesRecommendationsServiceImpl implements CategoriesRecommend
 
                     if (col.equals("_id")) continue;
                     else if (col.equals("mainCategory")) continue;
+                    else if (col.equals("rating")) continue;
 
                     FileOutputStream countFileOutputStream = new FileOutputStream(countingFile);
                     int cnt=0;
@@ -254,7 +261,7 @@ public class CategoriesRecommendationsServiceImpl implements CategoriesRecommend
                     int threeCnt = 0;
                     List<Row> rowList = ds.select(col).collectAsList();
                     for (int i = 0; i < rowList.size(); i++) {
-                        String s = rowList.get(i).get(0).toString();
+                        String s = rowList.get(i).get(0).toString().substring(0, 1);
                         if (!categoryList.get(i).equals(mainCategory)) continue;
                         cnt++;
                         if (s.equals("0")) zeroCnt++;
@@ -278,15 +285,20 @@ public class CategoriesRecommendationsServiceImpl implements CategoriesRecommend
                     JavaRDD<String[]> rawRatings = rawData.map(str -> str.split(" "));
                     JavaRDD<Rating> ratings = rawRatings.map(arr -> new Rating(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]), Double.parseDouble(arr[2])));
                     ratings.cache();
-                    MatrixFactorizationModel model = ALS.train(JavaRDD.toRDD(ratings), 3, 1, 0.01);
+                    MatrixFactorizationModel model = ALS.train(JavaRDD.toRDD(ratings), 1, 1, 0.01);
 
                     int K = 3;
                     Rating[] topKRecs = model.recommendProducts(1, K);
                     String s = "";
                     int num = topKRecs[0].product();
-                    if (num == 1) s = "low";
+                    if(topKRecs[0].rating() == topKRecs[1].rating()){
+
+                        num = Math.max(topKRecs[0].product(), topKRecs[1].product());
+                    }
+                    if( num==0) continue;
+                    else if (num == 1) s = "low";
                     else if (num == 2) s = "middle";
-                    else s = "high";
+                    else if(num==3)s = "high";
                     returnCountingList.add(CategoriesDto.of(col, s));
 
                 }
