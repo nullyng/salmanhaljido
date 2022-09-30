de4c7a9db2a1bafe5a5b37882bf337a5e7dff0ce
이기종 <rl00822@naver.com>
2022-09-30 15:16:43 +0900

    [fix] : news 검색 query문 수정

--- back/demo/src/main/java/com/salmanhaljido/demo/domain/news/repository/NewsRepository.java
+++ back/demo/src/main/java/com/salmanhaljido/demo/domain/news/repository/NewsRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
     Page<News> findAllByCategory(Category category, Pageable pageable);
-    @Query(value = "SELECT * FROM news n WHERE n.category = 'LIFE_AND_HEALTH' And (n.title LIKE '%시험%' OR n.summary LIKE '%시험%' OR n.title LIKE '%시험%' OR n.summary LIKE '%시험%') ORDER BY n.id"
+    @Query(value = "SELECT * FROM news n WHERE n.category = :category And (n.title LIKE :sido OR n.summary LIKE :sido OR n.title LIKE :sigungu OR n.summary LIKE :sigungu) ORDER BY n.id"
             ,countQuery = "SELECT COUNT(*) FROM news",
             nativeQuery = true)
-    Page<News> findAllByCategoryOrTitleOrSummaryWithPagination(Category category, String sido, String sigungu, Pageable pageable);
+    Page<News> findAllByCategoryOrTitleOrSummaryWithPagination(@Param("category") Category category,@Param("sido") String sido,@Param("sigungu") String sigungu, Pageable pageable);
 }
