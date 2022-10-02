1008f6c0e0bba3cc4e870ae171cb45ca00a8154c
nullyng <nullyng@gmail.com>
2022-10-03 00:19:36 +0900

    S07P22D110-85 [feat] : 검색 결과가 없는 경우 문구 출력
    - 검색 결과가 없는 경우 '검색 결과가 없습니다.' 문구 출력

--- src/components/Board/Search/SearchResult.js
+++ src/components/Board/Search/SearchResult.js
@@ -1,4 +1,4 @@
-import { Pagination, ThemeProvider } from "@mui/material";
+import { IconButton, Pagination, ThemeProvider, Tooltip } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { useCallback, useEffect, useState } from "react";
 
@@ -6,6 +6,7 @@ import theme from "components/common/theme";
 import ArticleCard from "components/Board/Article/ArticleCard";
 import { setNews, setTotalCount } from "modules/search";
 import { getSearchBoard } from "api/board";
+import { ArrowBack } from "@mui/icons-material";
 
 export const newsCategory = {
   PREGNANT_AND_DELIVERY: "임신/출산",
@@ -40,6 +41,11 @@ function SearchResult() {
     });
   }, [currentPage]);
 
+  // 뒤로 가기 버튼 클릭 시 전체 게시글 페이지로 이동
+  const handleBack = () => {
+    window.location.reload();
+  };
+
   useEffect(() => {
     fetchBoard();
   }, [currentPage, fetchBoard]);
@@ -48,26 +54,41 @@ function SearchResult() {
     <div className="search-result">
       <div className="search-result__wrapper">
         <div className="search-result__wrapper__top">
-          <div>
-            <span>{newsCategory[category]}</span>의 <span>"{input}"</span> 검색
-            결과
+          <div className="search-result__wrapper__top--title">
+            <IconButton onClick={handleBack}>
+              <ArrowBack />
+            </IconButton>
+            <div>
+              <span>{newsCategory[category]}</span>의 <span>"{input}"</span>
+              &nbsp;검색 결과
+            </div>
           </div>
-          <div>{totalCount}개</div>
+          <div className="search-result__wrapper__top--count">
+            {totalCount}개
+          </div>
         </div>
-        <div className="search-result__wrapper__content">
-          {news.map((value, idx) => (
-            <ArticleCard key={idx} {...value} />
-          ))}
-        </div>
-        <ThemeProvider theme={theme}>
-          <Pagination
-            count={Math.ceil(totalCount / 8)}
-            page={currentPage}
-            color="secondary"
-            className="pagenation"
-            onChange={onPageChange}
-          />
-        </ThemeProvider>
+        {news.length === 0 ? (
+          <p className="search-result__wrapper__content--no-data">
+            검색 결과가 없습니다.
+          </p>
+        ) : (
+          <div>
+            <div className="search-result__wrapper__content">
+              {news.map((value, idx) => (
+                <ArticleCard key={idx} {...value} />
+              ))}
+            </div>
+            <ThemeProvider theme={theme}>
+              <Pagination
+                count={Math.ceil(totalCount / 8)}
+                page={currentPage}
+                color="secondary"
+                className="pagenation"
+                onChange={onPageChange}
+              />
+            </ThemeProvider>
+          </div>
+        )}
       </div>
     </div>
   );
