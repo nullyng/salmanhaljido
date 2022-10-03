c625c4b7ab5d5776bd6bd3eabc490a7784f1ee53
nullyng <nullyng@gmail.com>
2022-10-03 22:24:09 +0900

    S07P22D110-137 [feat] : 데이터 입출력 틀 default를 열린 상태로 수정

--- src/components/Main/Input/Input.js
+++ src/components/Main/Input/Input.js
@@ -14,9 +14,7 @@ import MyCategory from "components/Main/Input/Category/MyCategory";
 import Category from "components/Main/Input/Category/Category";
 
 function Input() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
-  const myCategoryList = useSelector((state) => state.category.myCategoryList);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(true);
 
   const handleDrawerOpen = () => {
     setIsDrawerOpen(true);
